976c0b617af397c859e4178ba12dcaa71cfcbc3a
MelisaArce <95306750+MelisaArce@users.noreply.github.com>
2023-06-02 16:30:38 -0300

    actualizacion

--- challege/melisa_arce.py
+++ challege/melisa_arce.py
@@ -2,14 +2,27 @@ def ordenar_list(lista:list[int])-> list[int]:
     """
     Toma una lista por parametros y la devuelve ordenada con el algoritmo de burbuja.
     """
+        
     cant_elementos =len(lista) #devuelve el numero de elementos de la lista
 
     for i in range(cant_elementos-1):       
         for j in range(cant_elementos-1-i): 
             if lista[j] > lista[j+1]:
                 lista[j], lista[j+1] = lista[j+1], lista[j]
-
+        
     return lista
+
+def burbuja(lista:list[int]):
+    cant_elementos =len(lista) #devuelve el numero de elementos de la lista
+
+    for i in range(cant_elementos):       
+        for j in range(cant_elementos): 
+            if lista[j] > lista[i]:
+               # 5       >     1
+                aux = lista[j] # 5
+                lista[j]= lista[i] #1
+                lista[i]= aux #5
+            print(lista) 
 
 def factorial(numero:int)-> int:
     """
@@ -27,13 +40,24 @@ def serie_de_fibonacci(numero:int)->list[int]:
     """
     Toma un numero y devuelve una lista con la serie fibonacci
     """
-    if numero==1:
+    if numero==1 or numero == 0:
         return [0]
-    
+  
     lista_nueva= [0,1]
 
     for i in range(2, numero):
         lista_nueva.append(lista_nueva[-1]+ lista_nueva[-2])
 
-    return lista_nueva                   
+    return lista_nueva    
 
+lista_numeros = [5,1,14,46,7,10]   
+numero_ejemplo= 5
+numero_ejemplo_2=1    
+numero_ejemplo_3=0 
+
+print(ordenar_list(lista_numeros))
+#print(factorial(numero_ejemplo))
+#print(serie_de_fibonacci(numero_ejemplo))
+#print(serie_de_fibonacci(numero_ejemplo_2))
+#print(serie_de_fibonacci(numero_ejemplo_3))
+#burbuja(lista_numeros)
